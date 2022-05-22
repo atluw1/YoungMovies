b87545b47ffdcbdbce5cb0435a3b72483f479665
younghun <atluw@naver.com>
2022-05-23 02:17:29 +0900

    review 진행 중

---
+++ final-pjt-front/src/components/ReviewList.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-for>
+
+  </v-for>
+</template>
+
+<script>
+import axios from 'axios'
+
+// django 리뷰 주소
+let reviewUrl = "http://127.0.0.1:8000/api/v1/reviews/"
+
+export default {
+  name: 'ReviewList',
+  data: () => {
+    return {
+      reviewList: []
+    }
+  },
+  mounted() {
+    axios({
+      method: 'GET',
+      url: reviewUrl
+    })
+      .then(res => {
+        this.reviewList = res.data
+      })
+      .catch(res => {
+        console.log('불러오는 데 실패했습니다.', res)
+      })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ final-pjt-front/src/views/ReviewView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>모든 리뷰입니다</h1>
+    <div>
+      {{ reviews }}
+    </div>
+  </div>
+</template>
+
+<script>
+import ReviewList from '@/components/ReviewList.vue'
+
+export default {
+  name: 'ReviewView',
+  
+
+}
+</script>
+
+<style>
+
+</style>
