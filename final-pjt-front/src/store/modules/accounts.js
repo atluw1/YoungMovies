8970d41ccdc160140a91ca1ec724a3c480ad194d
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2022-05-22 13:51:07 +0900

    로그인 기능까지 완료

--- final-pjt-front/src/store/modules/accounts.js
+++ final-pjt-front/src/store/modules/accounts.js
@@ -18,14 +18,18 @@ export default {
     isLoggedIn: state => !!state.token,
     currentUser: state => state.currentUser,
     profile: state => state.profile,
-    authError: state => state.authError
+    authError: state => state.authError,
+    authHeader: state => ({ Authorization: `Token ${state.token}` })
   },
 
   mutations: {
     SET_TOKEN: (state, token) => state.token = token,
     SET_CURRENT_USER: (state, user) => state.currentUser = user,
     SET_PROFILE: (state, profile) => state.profile = profile,
+
     SET_AUTH_ERROR: (state, error) => state.authError = error,
+
+    // localstorage를 자동 state에 저장하는 라이브러리 사용 시 초기화가 되지 않는 문제 때문에 사용
     SET_AUTH_ERROR_NULL: (state) => state.authError = null,
   },
   actions: {
@@ -44,6 +48,25 @@ export default {
     set_auth_error_null ({commit}) {
       commit('SET_AUTH_ERROR_NULL')
     },
+    
+    login({ commit, dispatch }, credentials) {
+      axios({
+        url: drf.accounts.login(),
+        method: 'post',
+        data: credentials,
+      }).then(res => {
+        const token = res.data.key
+        dispatch('saveToken', token)
+        dispatch('fetchCurrentUser')
+        router.push({ name: 'HomeView' })
+      }).catch(err => {
+        console.error(err)
+        commit('SET_AUTH_ERROR', err.response.data)
+      })
+    },
+    logout({ getters, dispatch }) {
+      
+    },
 
     signup({ commit, dispatch }, credentials) {
       axios({
@@ -53,12 +76,35 @@ export default {
       }).then(res => {
         const token = res.data.key
         dispatch('saveToken', token)
+        dispatch('fetchCurrentUser')
         router.push({ name: 'HomeView' })
       }).catch(err => {
         console.error(err)
         commit('SET_AUTH_ERROR', err.response.data)
       })
+    },
+    // 만약 지금 정상적으로 로그인 된 상태라면(토큰이 있다면), 유저 정보를 받아 온다. 
+    // => 주소는 rest_framework에서 제공하는 user/로 보낸다.
+    // 실패하면(토큰이 만료 등으로 잘못되었다면) 기존 토큰을 삭제하고 LoginView로 이동
+    // 언제 쓰냐? => signup, login이 끝나면
+    fetchCurrentUser ({ commit, getters, dispatch }) {
+      if (getters.isLoggedIn) {
+        axios({
+          url: drf.accounts.currentUserInfo(),
+          method: 'get',
+          // 아래서 headers에서 s 빼먹었었다...
+          headers: getters.authHeader
+        }).then(res => {
+          commit('SET_CURRENT_USER', res.data)
+        }).catch(err => {
+          if (err.response.status === 401) {
+            dispatch('removeToken')
+            router.push({ name: 'login'})
+          }
+        })
+      }
     }
+
   },
 
 }
