94aff3dd04f7bb965edb50d122804771b245f456
younghun <atluw@naver.com>
2022-05-23 14:42:39 +0900

    검색 닫기 버튼 눌렀을 때도 초기화

--- final-pjt-front/src/components/ReviewList.vue
+++ final-pjt-front/src/components/ReviewList.vue
@@ -1,9 +1,10 @@
 <template>
-  
+  <div>
+    <p>번호 제목 작성자 작성일</p>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
 
 // django 리뷰 주소
 let reviewUrl = "http://127.0.0.1:8000/api/v1/reviews/"
@@ -19,7 +20,7 @@ export default {
   },
   methods: {
     toReviewDetail: function() {
-      this.$router.push({ name: 'DetailView', params: { movieId: this.movie.id } })
+      this.$router.push({ name: 'ReviewDetail', params: { review_pk: this.review.pk } })
     }
   }
 }
