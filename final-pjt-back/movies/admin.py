ed1577bf89e64f9f95879514dc70d038cceff177
Cr-Mo-Marco-3000 <bizyoung93@gmail.com>
2022-05-26 23:02:48 +0900

    admin 끝

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Movie, Movie_score
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'poster_path', 'tmdb_id',)
+
+class Movie_scoreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'score', 'movie_id', 'user_id')
+
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Movie_score, Movie_scoreAdmin)
--- final-pjt-back/reviews/admin.py
+++ final-pjt-back/reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Review, Comment
 
 # Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'content','created_at','updated_at','user')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'content', 'review', 'user', 'created_at', 'updated_at')
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
